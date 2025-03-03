27fa2f469d5f27c437358a93c5b3b84bade1a7ba
Priyanshi Trivedi <priyanshitrivedi215@gmail.com>
2025-03-03 23:50:15 +0530

    Time: 12 ms (25.48%), Space: 141.1 MB (11.34%) - LeetHub

---
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+          vector<int> less, equal, greater;
+    
+    // Partition elements into three separate vectors
+    for (int num : nums) {
+        if (num < pivot) less.push_back(num);
+        else if (num == pivot) equal.push_back(num);
+        else greater.push_back(num);
+    }
+    
+    // Merge the three partitions
+    vector<int> result;
+    result.insert(result.end(), less.begin(), less.end());
+    result.insert(result.end(), equal.begin(), equal.end());
+    result.insert(result.end(), greater.begin(), greater.end());
+    
+    return result;
+    }
+};
