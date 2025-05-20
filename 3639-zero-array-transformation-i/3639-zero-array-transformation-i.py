fcc33de4db78b3ec8d6191f4b1cc05c6f5c2fd9e
Priyanshi Trivedi <priyanshitrivedi215@gmail.com>
2025-05-21 00:20:42 +0530

    Time: 79 ms (65.99%), Space: 55.1 MB (39%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        n = len(nums)
+        count = [0] * (n + 1)
+        for l, r in queries:
+            count[l] += 1
+            if r + 1 < n:
+                count[r + 1] -= 1
+        for i in range(1, n):
+            count[i] += count[i - 1]
+        for i in range(n):
+            if count[i] < nums[i]:
+                return False
+        return True
+
+        
